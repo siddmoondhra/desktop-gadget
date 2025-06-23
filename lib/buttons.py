d2d0f48081a4ee713c8a7f3d80e1029316655f90
siddmoondhra <sid.moondhra@gmail.com>
2025-06-22 21:03:10 -0700

    Fixed button handling so internal resistors are used

--- lib/buttons.py
+++ lib/buttons.py
@@ -1,37 +1,24 @@
-import RPi.GPIO as GPIO
+import digitalio
+import board
 import time
 
 class ButtonHandler:
     def __init__(self, config):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        
-        self.button_pins = {
-            'up': config.BUTTON_UP,
-            'down': config.BUTTON_DOWN,
-            'select': config.BUTTON_SELECT,
-            'back': config.BUTTON_BACK
+        self.buttons = {
+            'up': self._setup_button(config.BUTTON_UP),
+            'down': self._setup_button(config.BUTTON_DOWN),
+            'select': self._setup_button(config.BUTTON_SELECT),
+            'back': self._setup_button(config.BUTTON_BACK)
         }
         
-        # Setup buttons with pull-up resistors
-        for pin in self.button_pins.values():
-            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        
-        self.last_press_time = {}
-        self.debounce_delay = 0.2
+    def _setup_button(self, pin):
+        button = digitalio.DigitalInOut(getattr(board, f"D{pin}"))
+        button.direction = digitalio.Direction.INPUT
+        button.pull = digitalio.Pull.UP  
         
     def get_pressed(self):
-        current_time = time.time()
-        
-        for name, pin in self.button_pins.items():
-            # Button is pressed when pin reads LOW (due to pull-up)
-            if not GPIO.input(pin):
-                # Check debounce
-                if name not in self.last_press_time or \
-                   (current_time - self.last_press_time[name]) > self.debounce_delay:
-                    self.last_press_time[name] = current_time
-                    return name
+        for name, button in self.buttons.items():
+            if not button.value:  
+                time.sleep(0.1)  # debounce
+                return name
         return None
-    
-    def cleanup(self):
-        GPIO.cleanup()
